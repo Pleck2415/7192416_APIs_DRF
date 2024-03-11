de536415fb570dee029049772151b2f25d11fb50
Patrick Leclerc <patrick.leclerc14@gmail.com>
2024-03-11 19:22:49 -0400

    product in category

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,4 +1,9 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from shop.models import Category, Product, Article
+class ArticleSerializer(ModelSerializer):
+
  
 from shop.models import Category
 from shop.models import Product
@@ -7,13 +12,18 @@ from shop.models import Article
 class CategorySerializer(ModelSerializer):
  
     class Meta:
+        model = Article
+        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
         model = Category
         fields = ['id', 'date_created', 'date_updated', 'name', 'active']
 
 class ProductSerializer(ModelSerializer):
-
     class Meta:
         model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+class CategorySerializer(serializers.ModelSerializer):
+
+    products = serializers.SerializerMethodField()
         fields = ['id', 'date_created', 'date_updated', 'name', 'category', 'active']
 
 class ArticleSerializer(ModelSerializer):
@@ -21,3 +31,12 @@ class ArticleSerializer(ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'date_created', 'date_updated', 'name', 'description', 'price', 'product', 'active']
+
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+
+        def get_products(self, instance):
+
+            queryset = instance.products.filter(active=True)
+            serializer = ProductSerializer(queryset, many=True)
+            return serializer.data
